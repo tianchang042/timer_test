81a64f70a785f03227b173c742ae77e10b85f357
tianchang042 <416613474@qq.com>
2018-05-16 11:15:23 +0800

    test_2
    sdsss

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Timer_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnControl = new System.Windows.Forms.Button();
+            this.btnMan = new System.Windows.Forms.Button();
+            this.btnOther = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnControl
+            // 
+            this.btnControl.Location = new System.Drawing.Point(321, 80);
+            this.btnControl.Name = "btnControl";
+            this.btnControl.Size = new System.Drawing.Size(75, 23);
+            this.btnControl.TabIndex = 0;
+            this.btnControl.Text = "AUTOCONTROL";
+            this.btnControl.UseVisualStyleBackColor = true;
+            this.btnControl.Click += new System.EventHandler(this.btnControl_Click);
+            // 
+            // btnMan
+            // 
+            this.btnMan.Location = new System.Drawing.Point(321, 135);
+            this.btnMan.Name = "btnMan";
+            this.btnMan.Size = new System.Drawing.Size(75, 23);
+            this.btnMan.TabIndex = 1;
+            this.btnMan.Text = "Manual";
+            this.btnMan.UseVisualStyleBackColor = true;
+            this.btnMan.Click += new System.EventHandler(this.btnMan_Click);
+            // 
+            // btnOther
+            // 
+            this.btnOther.Location = new System.Drawing.Point(154, 135);
+            this.btnOther.Name = "btnOther";
+            this.btnOther.Size = new System.Drawing.Size(75, 23);
+            this.btnOther.TabIndex = 2;
+            this.btnOther.Text = "Other";
+            this.btnOther.UseVisualStyleBackColor = true;
+            this.btnOther.Click += new System.EventHandler(this.btnOther_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOther);
+            this.Controls.Add(this.btnMan);
+            this.Controls.Add(this.btnControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnControl;
+        private System.Windows.Forms.Button btnMan;
+        private System.Windows.Forms.Button btnOther;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace Timer_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Timers.Timer t = new System.Timers.Timer(5000);
+        private void btnControl_Click(object sender, EventArgs e)
+        {
+            t.Elapsed += new System.Timers.ElapsedEventHandler(theout);
+            t.AutoReset = true;
+            t.Enabled = true;
+            btnOther.Enabled = false;
+        }
+        public void theout(object source, System.Timers.ElapsedEventArgs e)
+        {
+            MessageBox.Show("OK!");
+        }
+
+        private void btnMan_Click(object sender, EventArgs e)
+        {
+            btnOther.Enabled = true;
+            t.Stop();
+            MessageBox.Show("stop");
+            
+        }
+
+        private void btnOther_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("other");
+        }
+    }
+}
